2ce0d7582d8b392fd958487251db7cf534c97b27
Waltythan <tsaqiif26@gmail.com>
2026-04-06 08:25:37 +0700

    Test gagal format test

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
-def tambah(a,  b): # Tambah dua spasi di sini
-    return a+b     # Hapus spasi di sekitar +
+def tambah(a, b):
+    return a + b
 
 
 if __name__ == "__main__":
+    angka_rahasia = 100 # Buat variabel tapi jangan dipakai
     print(f"Hasil: {tambah(10, 5)}")
