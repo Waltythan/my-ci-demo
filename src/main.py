1eea944d4ae03409083408c1f1b46a34894eb199
Waltythan <tsaqiif26@gmail.com>
2026-04-06 08:32:58 +0700

    Test gagal lint test

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 def tambah(a, b):
     return a + b
 
+
 if __name__ == "__main__":
-    angka_rahasia = 100 # Buat variabel tapi jangan dipakai
+    angka_rahasia = 100  # Buat variabel tapi jangan dipakai
     print(f"Hasil: {tambah(10, 5)}")
