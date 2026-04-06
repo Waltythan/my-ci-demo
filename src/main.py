dcd7d7a844268e0602bce3ff21ea6e21a4d4a4ab
Waltythan <tsaqiif26@gmail.com>
2026-04-06 08:49:20 +0700

    Test gagal unit test

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 def tambah(a, b):
-    return a + b
+    return a - b
 
 
 if __name__ == "__main__":
-    # Niatnya manggil fungsi tambah, tapi malah ngetik variabel yang nggak ada
-    print(f"Hasil: {hasil_penjumlahan}")
+    print(f"Hasil: {tambah(10, 5)}")
